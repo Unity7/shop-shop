e55cdda0da8cd4fae6ef221820556a46fc8e8e22
Bay Bui <baybui@ucla.edu>
2021-07-11 15:18:30 -0700

    upated cart

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -10,6 +10,8 @@ const StoreProvider = ({ value = [], ...props }) => {
   //dispatch method we execute to update our state. It is specifically going to look for an action object passed in as its argument
   const [state, dispatch] = useProductReducer({
     products: [],
+    cart: [],
+    cartOpen: false,
     categories: [],
     currentCategory: "",
   });
